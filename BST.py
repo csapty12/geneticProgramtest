896727d62670fff9addbe806b56520c287eb44f2
csapty12 <csapty12@gmail.com>
2017-03-01 14:06:34 +0000

    BST node tracker complete. Function to make now is the random node selector for testing before performing crossover

--- BST.py
+++ BST.py
@@ -1,33 +1,40 @@
 import sys
-from random import randint,choice,random,sample
+from random import randint, choice, random, sample
 from math import log
 import numpy as np
 import re
-
 
 OPS = ['+', '-', '*']
 GROUP_PROB = 0.3
 MIN_NUM, MAX_NUM = 0, 20
 
-
 inputs = [4, 8, 12, 13]
 output = [60, 64, 68, 69]
 ideal_solution = "x1+8*(4+3)"
 randomVariable1 = [randint(MIN_NUM, MAX_NUM), "X1"]
+num_nodes = 0
+
 
 class Tree(object):
+    # every instance of a tree is a node.
+    def __init__(self, val, left=None, right=None, nodesNumber =0, glob=True):
+        if glob ==True:
+            global num_nodes
+            num_nodes+=1
+            self.nodeID = num_nodes
+            self.val = val  # holds the value
+            self.left = left  # holds the left child value
+            self.right = right  # holds the right child value
+        else:
+            self.nodeID = nodesNumber
+            self.val = val  # holds the value
+            self.left = left  # holds the left child value
+            self.right = right  # holds the right child value
 
-	#every instance of a tree is a node.  
-	def __init__(self,val, left = None, right = None, track_val = None ):
-		self.val = val # holds the value
-		self.left= left # holds the left child value
-		self.right = right # holds the right child value
-		self.track_val = track_val
-		
 
-	def __str__(self):
+def __str__(self):
+    return str(self.val)  # print out value of node
 
-		return str(self.val) #print out value of node 
 
 #########################################################################################
 
@@ -52,7 +59,7 @@ class GenExp:
 
         if depth < maxdepth and randint(0, maxdepth) > depth:
             self.right = GenExp(maxNumbers, maxdepth,
-                                        depth + 1)  # generate part of the expression (on the right)
+                                depth + 1)  # generate part of the expression (on the right)
         else:
             self.right = randint(MIN_NUM, MAX_NUM)
 
@@ -65,7 +72,6 @@ class GenExp:
             return '({0})'.format(s)
         else:
             return s
-
 
     def get_valid_expressions(self, maxNumbers, populationSize):
         expression_list = []
@@ -88,11 +94,9 @@ class GenExp:
             eval_list.append(new_exps)
         return eval_list
 
-
-
     def get_totals(self, expression):
         """
-		evaluate the expression and give the output. 
+        evaluate the expression and give the output.
         """
         totals = []
         for i in expression:
@@ -102,7 +106,6 @@ class GenExp:
                 temp_totals.append(x)
             totals.append(temp_totals)
         return totals
-
 
     def get_mean_squared_fitness(self, differences):
         mean_sq = []
@@ -121,208 +124,216 @@ class GenExp:
             mean.append(x)
         return mean
 
-
     def select_parents(self, population, fitness, num_parents):
         print("population: ", population)
         parents = sample(population, num_parents)
 
-
         return parents
 
-    def split_parents(self,parents):
-    	split_list = [re.findall('\w+|\W', s) for s in parents]
+    def split_parents(self, parents):
+        split_list = [re.findall('\w+|\W', s) for s in parents]
 
-    	for i in split_list:
-    		i.append("end")
-    	return split_list
+        for i in split_list:
+            i.append("end")
+        return split_list
 
-    def track_nodes(self,parents):
-    	newl = []
-    	for i in parents:
-    		x = i.split()
-    		newl.append(x)
-    	# print(newl)
+        # def track_nodes(self,parents):
+        #   newl = []
+        #   for i in parents:
+        #       x = i.split()
+        #       newl.append(x)
+        #   # print(newl)
 
-    	new2 = []
-    	for i in range(len(newl)):
-    		tmp = []
-    		[tmp.append(newl[i][j]) for j in range(len(newl[i]))]
-    		new2.append(tmp)
-    	
+        #   new2 = []
+        #   for i in range(len(newl)):
+        #       tmp = []
+        #       [tmp.append(newl[i][j]) for j in range(len(newl[i]))]
+        #       new2.append(tmp)
 
-    	[new2[i].remove(new2[i][j]) for i in range(len(new2)) for j in range(len(new2[i])) if ' ' in new2[i][j]]
 
-    	vals = []
-    	for i in range(len(new2)):
-    		tmp =[]
-    		[tmp.append(j+1) for j in range(len(new2[i]))]
-    		vals.append(tmp)
+        #   [new2[i].remove(new2[i][j]) for i in range(len(new2)) for j in range(len(new2[i])) if ' ' in new2[i][j]]
 
-    	tups = []
-    	for i in range(len(new2)):
-    		tmp = []
-    		[tmp.append(j)for j in zip(new2[i],vals[i])]
-    		tups.append(tmp)
-    	print(tups)
+        #   vals = []
+        #   for i in range(len(new2)):
+        #       tmp =[]
+        #       [tmp.append(j+1) for j in range(len(new2[i]))]
+        #       vals.append(tmp)
 
-    	
+        #   tups = []
+        #   for i in range(len(new2)):
+        #       tmp = []
+        #       [tmp.append(j)for j in zip(new2[i],vals[i])]
+        #       tups.append(tmp)
+        #   print(tups)
 
-#print out the expression in prefix notation -> express 1+2*3
+
+
+
+# print out the expression in prefix notation -> express 1+2*3
 def print_tree_prefix(tree):
-	if tree.left ==None:
-		sys.stdout.write("%s " %(tree.val))
-		return tree.val
-	else:
-		sys.stdout.write("%s " %(tree.val))
-		left = print_tree_prefix(tree.left)
-		right = print_tree_prefix(tree.right)
-		return tree.val + " "+ left+' '+ right + ''
+    if tree.left == None:
+        sys.stdout.write("%s " % (tree.val))
+        return tree.val
+    else:
+        sys.stdout.write("%s " % (tree.val))
+        left = print_tree_prefix(tree.left)
+        right = print_tree_prefix(tree.right)
+        return tree.val + " " + left + ' ' + right + ''
+
+
 
 def print_tree_postfix(tree):
-	if tree.left ==None:
-		return tree.val
-	else:
-		print_tree_postfix(tree.left)
-		print_tree_postfix(tree.right)
-		sys.stdout.write("%s " %(tree.val))
+    if tree.left == None:
+        return tree.val
+    else:
+        print_tree_postfix(tree.left)
+        print_tree_postfix(tree.right)
+        sys.stdout.write("%s " % (tree.val))
+
 
 def print_tree_inorder(tree):
-	if tree.left == None:
-		return tree.val
-	else:
+    if tree.left == None:
+        return tree.val
+    else:
 
-		left = print_tree_inorder(tree.left)
+        left = print_tree_inorder(tree.left)
 
-		# sys.stdout.write("%s " %(tree.val))
+        right = print_tree_inorder(tree.right)
 
-		right = print_tree_inorder(tree.right)
-
-		return left + tree.val + right
+        return left + tree.val + right
 
 
+def show_tree(tree, level=0):
 
-def show_tree(tree, level = 0):
+    if tree == None:
+        return
+    else:
+        show_tree(tree.right, level + 1)
+        print('  ' * level + str(tree.val) + "   "+ str(tree.nodeID))
+        show_tree(tree.left, level + 1)
 
-	if tree == None: 
-		return 
-	else:
-		show_tree(tree.right,level+1)
-		print('  '*level + str(tree.val))
-		show_tree(tree.left,level+1)
 
-def get_operation(token_list,expected):
-	"""
-	compares the expected token to the first token on the list. if they match, remove it, return True
-	this is to get the operator
-	"""
-	if token_list[0] ==expected:
-		del  token_list[0]
-		return True
-	else:
-		return False
 
-def get_number(token_list, t_val):
+
+def get_nodes(tree, level = 0):
+    l1 = []
+    if type(tree) == type(None):
+        return []
+    else:
+        right = get_nodes(tree.right, level + 1)
+        
+        if right == None:
+            right = []
+        # print("xx:  ",str(tree.val) + " " +str(tree.nodeID))
+        left = get_nodes(tree.left, level + 1)
+        if left == None:
+            left = []
+        l1 += right + left 
+        l1.append((Tree(tree.val,None, None,tree.nodeID, False)))
+        return l1
+
+        
+
+
+
+
+
+def get_operation(token_list, expected):
+    """
+    compares the expected token to the first token on the list. if they match, remove it, return True
+    this is to get the operator
+    """
+    if token_list[0] == expected:
+        del token_list[0]
+        return True
+    else:
+        return False
+
+
+def get_number(token_list):
     if get_operation(token_list, '('):
-        x = get_expression(token_list)         # get the subexpression
-        get_operation(token_list, ')')      # remove the closing parenthesis
+        x = get_expression(token_list)  # get the subexpression
+        get_operation(token_list, ')')  # remove the closing parenthesis
         return x
     else:
         x = token_list[0]
         if type(x) != type("o"): return None
         token_list[0:1] = []
-        print(Tree (x, None, None, track_val = t_val+1))
-        return Tree (x, None, None, track_val = t_val+1)
+        return Tree(x, None, None)
 
 
-
-def get_product(token_list, t_val):
-    a = get_number(token_list, t_val)
+def get_product(token_list):
+    a = get_number(token_list, )
     if get_operation(token_list, '*'):
-        b = get_product(token_list, t_val+1)
-        print(Tree ('*', a, b, track_val = t_val+1))
-        return Tree ('*', a, b, track_val = t_val+1)
+        b = get_product(token_list)
+        return Tree('*', a, b)
     else:
         return a
 
 
-def get_expression(token_list, t_val=0):
-	val =0
-	a = get_product(token_list, t_val+1)
-	if get_operation(token_list, '-'):
-		b = get_expression(token_list)
-		print(Tree ('-', a, b, track_val = t_val+1))
-		return Tree ('-', a, b, track_val = t_val+1)
-	elif get_operation(token_list, '+'):
-		b = get_expression(token_list)
-		print(Tree ('+', a, b, track_val = t_val+1))
-		return Tree ('+', a, b, track_val = t_val+1)
-	else:
-		return a
+def get_expression(token_list):
+    a = get_product(token_list)
+    if get_operation(token_list, '-'):
+        b = get_expression(token_list)
+        return Tree('-', a, b, )
+    elif get_operation(token_list, '+'):
+        b = get_expression(token_list)
+        return Tree('+', a, b)
+    else:
+        return a
+
 
 def get_prefix_expressions(token_list):
-	my_list = []
-	tree_obj = []
-	
-	for i in token_list:
+    my_list = []
+    tree_obj = []
 
-		tree = get_expression(i)
+    for i in token_list:
+        tree = get_expression(i)
+        
+        y = print_tree_prefix(tree)
+        print('\n')
+        my_list.append(y)
 
-		y = print_tree_prefix(tree)
-		print('\n')
-		tree_obj.append(tree)
-		my_list.append(y)
+        print()
+        print()
+        show_tree(tree)
+        print()
+        print()
+        x = get_nodes(tree)
+        for i in x:
+            print(i.val,i.nodeID)
+        print()
+        print()
 
-		print()
-		print()
-		show_tree(tree)
-		print()
-		print()
+    return my_list
 
-
-			
-	
-	return my_list
-
-def get_nodes(token_list):
-	print("token list: ", token_list)
-	for i in token_list:
-		
-		tree = get_expression(i)
-
-		
 
 
 def main2():
-	print("======================================================")
-	print("Aim to find a function to map my inputs to my outputs.")
-	print("inputs: ", inputs)
-	print("outputs: ", output)
-	print("======================================================")
-	test = GenExp(4)
-	generate_expressions = test.get_valid_expressions(4, 4)  # (maxNumber,Population size)
-	print("Population: ", generate_expressions)
+    print("======================================================")
+    print("Aim to find a function to map my inputs to my outputs.")
+    print("inputs: ", inputs)
+    print("outputs: ", output)
+    print("======================================================")
+    test = GenExp(4)
+    generate_expressions = test.get_valid_expressions(4, 4)  # (maxNumber,Population size)
+    print("Population: ", generate_expressions)
 
-	eval_exp = test.eval_expressions(generate_expressions)
-	get_totals = test.get_totals(eval_exp)
-	print("totals: ", get_totals)
-	get_fitness = test.get_mean_squared_fitness(get_totals)
-	print("fitness error: ",get_fitness)
-	print()
-	print("=======================================================")
-	print("parents")
-	select_parents = test.select_parents(generate_expressions, get_fitness, 2)
-	print("parents selected: ",select_parents)
-	split_parents = test.split_parents(select_parents)
-	# print("split parents: ", split_parents)
-	# prefix_exp = get_prefix_expressions(split_parents)
-	# print(prefix_exp)
-	print('\n \n \n \n \n ')
-	nodes = get_nodes(split_parents)
-	# track_nodes = test.track_nodes(prefix_exp)
-	
-
-	
-
-
-if __name__=="__main__":
-	main2()
+    eval_exp = test.eval_expressions(generate_expressions)
+    get_totals = test.get_totals(eval_exp)
+    print("totals: ", get_totals)
+    get_fitness = test.get_mean_squared_fitness(get_totals)
+    print("fitness error: ", get_fitness)
+    print()
+    print("=======================================================")
+    print("parents")
+    select_parents = test.select_parents(generate_expressions, get_fitness, 2)
+    print("parents selected: ", select_parents)
+    split_parents = test.split_parents(select_parents)
+    # print("split parents: ", split_parents)
+    prefix_exp = get_prefix_expressions(split_parents)
+    print(prefix_exp)
+    print('\n \n \n \n \n ')
+        
+if __name__ == "__main__":
+    main2()
